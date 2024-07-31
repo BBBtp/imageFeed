a7ba92dfa7562e79386cd2bb3f3cea0676452568
Bogdan <btoporin@mail.ru>
2024-07-31 19:29:10 +0300

    Добавлен экран для масштабирования картинки, сверстан экран для профиля, добавлена кнопка шеринга

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -14,9 +15,25 @@ final class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+                    guard
+                        let viewController = segue.destination as? SingleImageViewController,
+                        let indexPath = sender as? IndexPath
+                    else {
+                        assertionFailure("Invalid segue destination")
+                        return
+                    }
+
+                    let image = UIImage(named: photosName[indexPath.row])
+                    _ = viewController.view 
+            viewController.image = image
+                } else {
+                    super.prepare(for: segue, sender: sender)
+                }
     }
 }
 
@@ -55,6 +72,9 @@ extension ImagesListViewController {
 
 extension ImagesListViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else{
